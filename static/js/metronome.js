97c10684bfb872af6401edee62f4a89da2543466
joelytin <joelytin@outlook.com>
2024-10-30 10:29:19 +0800

    Add styles

---
+++ static/js/metronome.js
@@ -0,0 +1,40 @@
+const tempoDisplay = document.querySelector('.tempo');
+const tempoText = document.querySelector('.tempo-text');
+const decreaseTempoBtn = document.querySelector('.decrease-tempo');
+const increaseTempoBtn = document.querySelector('.increase-tempo');
+const tempoSlider = document.querySelector('.slider');
+const startStopBtn = document.querySelector('.start-stop');
+const subtractBeats = document.querySelector('.subtract-beats');
+const addBeats = document.querySelector('.add-beats');
+const measureCount = document.querySelector('.measure-count');
+
+let bpm = 140;
+let beatsPerMeasure = 4;
+
+decreaseTempoBtn.addEventListener('click', () => {
+    if (bpm < 20) return;
+    bpm --;
+    tempoDisplay.textContent = bpm;
+    tempoSlider.value = bpm;
+});
+increaseTempoBtn.addEventListener('click', () => {
+    if (bpm > 280) return;
+    bpm ++;
+    tempoDisplay.textContent = bpm;
+    tempoSlider.value = bpm;
+});
+tempoSlider.addEventListener('input', () => {
+    bpm = tempoSlider.value;
+    tempoDisplay.textContent = bpm;
+    tempoSlider.value = bpm;
+
+});
+
+subtractBeats.addEventListener('click', () => {
+    beatsPerMeasure--;
+    measureCount.textContent = beatsPerMeasure;
+});
+addBeats.addEventListener('click', () => {
+    beatsPerMeasure++;
+    measureCount.textContent = beatsPerMeasure;
+});

a760d7bd26742cc2fafa8d0edaa2cea4ccac6bad
joelytin <joelytin@outlook.com>
2025-03-09 19:00:37 +0800

    Add amplified metronome tick

--- static/js/metronome.js
+++ static/js/metronome.js
@@ -11,8 +11,8 @@ const subtractBeats = document.querySelector('.subtract-beats');
 const addBeats = document.querySelector('.add-beats');
 const measureCount = document.querySelector('.measure-count');
 
-const click1 = new Audio('static/audio/Perc_MetronomeQuartz_hi.mp3');
-const click2 = new Audio('static/audio/Perc_MetronomeQuartz_lo.mp3');
+const click1 = new Audio('static/audio/Perc_MetronomeQuartz_hi2.mp3');
+const click2 = new Audio('static/audio/Perc_MetronomeQuartz_lo2.mp3');
 
 let bpm = 140;
 let beatsPerMeasure = 4;
